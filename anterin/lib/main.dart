a5896fd893d5596c4c4b37f9f938d844709eee6f
VincentWu503 <vincenthendry06@gmail.com>
2025-09-25 20:28:38 +0700

    pemindahan

--- anterin/lib/main.dart
+++ anterin/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'login-screens/start.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:anterin/routes/main_router.dart';
 
@@ -12,13 +12,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Anterin App',
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const StartPage(),
+      theme: _buildTheme(Brightness.light),
+      routerConfig: _initializeRoute(),
     );
   }
+}
+
+GoRouter _initializeRoute() {
+  final MainRouter mainRoute = MainRouter();
+  GoRouter mainRouter = mainRoute.mainRouter();
+  return mainRouter;
+}
+
+ThemeData _buildTheme(Brightness brightness) {
+  final ThemeData baseTheme = ThemeData(brightness: brightness);
+
+  return baseTheme.copyWith(
+    colorScheme: ColorScheme.fromSeed(seedColor: Color(0x00008b4e)),
+    textTheme: GoogleFonts.latoTextTheme(baseTheme.textTheme),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color.fromARGB(255, 130, 255, 161),
+      foregroundColor: Colors.white,
+    ),
+  );
 }
--- anterin/lib/routes/main_router.dart
+++ anterin/lib/routes/main_router.dart
@@ -9,72 +9,74 @@ final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>();
 // );
 
 // RETURN SCREEN DI SINI
-GoRouter mainRouter() {
-  final mainRouter = GoRouter(
-    navigatorKey: _rootNavigatorKey,
-    initialLocation: '/home',
-    routes: [
-      ShellRoute(
-        // navigatorKey: _shellNavigatorKey,
-        builder: (context, state, child) {
-          final location = state.uri.toString();
-          print(location);
-          return AnterinTabBar(location: location, child: child);
-        },
-        routes: [
-          GoRoute(
-            path: '/home',
-            name: 'home',
-            builder: (context, state) => const HomeScreen(),
-            routes: [
-              GoRoute(
-                path: 'motorcycle',
-                name: 'motorcycle',
-                // entah kenapa parentNavigatorKey exception, padahal ngikutin blog orang
-                // parentNavigatorKey: _shellNavigatorKey,
-                builder: (context, state) {
-                  print(state.uri.toString());
-                  return const Placeholder();
-                },
-              ),
-              GoRoute(
-                path: 'car',
-                name: 'car',
-                builder: (context, state) {
-                  print(state.uri.toString());
-                  return const Placeholder();
-                },
-              ),
-              GoRoute(
-                path: 'delivery',
-                name: 'delivery',
-                // entah kenapa parentNavigatorKey exception, padahal ngikutin blog orang
-                // parentNavigatorKey: _shellNavigatorKey,
-                builder: (context, state) {
-                  print(state.uri.toString());
-                  return const Placeholder();
-                },
-              ),
-            ],
-          ),
-          GoRoute(
-            path: '/activity',
-            name: 'activity',
-            builder: (context, state) => const Placeholder(),
-          ),
-          GoRoute(
-            path: '/chat',
-            name: 'chat',
-            builder: (context, state) => const Placeholder(),
-          ),
-          GoRoute(
-            path: '/profile',
-            name: 'profile',
-            builder: (context, state) => const Placeholder(),
-          ),
-        ],
-      ),
-    ],
-  );
-  return mainRouter;
+class MainRouter {
+  GoRouter mainRouter() {
+    final mainRouter = GoRouter(
+      navigatorKey: _rootNavigatorKey,
+      initialLocation: '/home',
+      routes: [
+        ShellRoute(
+          // navigatorKey: _shellNavigatorKey,
+          builder: (context, state, child) {
+            final location = state.uri.toString();
+            print(location);
+            return AnterinTabBar(location: location, child: child);
+          },
+          routes: [
+            GoRoute(
+              path: '/home',
+              name: 'home',
+              builder: (context, state) => const HomeScreen(),
+              routes: [
+                GoRoute(
+                  path: 'motorcycle',
+                  name: 'motorcycle',
+                  // entah kenapa parentNavigatorKey exception, padahal ngikutin blog orang
+                  // parentNavigatorKey: _shellNavigatorKey,
+                  builder: (context, state) {
+                    print(state.uri.toString());
+                    return const Placeholder();
+                  },
+                ),
+                GoRoute(
+                  path: 'car',
+                  name: 'car',
+                  builder: (context, state) {
+                    print(state.uri.toString());
+                    return const Placeholder();
+                  },
+                ),
+                GoRoute(
+                  path: 'delivery',
+                  name: 'delivery',
+                  // entah kenapa parentNavigatorKey exception, padahal ngikutin blog orang
+                  // parentNavigatorKey: _shellNavigatorKey,
+                  builder: (context, state) {
+                    print(state.uri.toString());
+                    return const Placeholder();
+                  },
+                ),
+              ],
+            ),
+            GoRoute(
+              path: '/activity',
+              name: 'activity',
+              builder: (context, state) => const Placeholder(),
+            ),
+            GoRoute(
+              path: '/chat',
+              name: 'chat',
+              builder: (context, state) => const Placeholder(),
+            ),
+            GoRoute(
+              path: '/profile',
+              name: 'profile',
+              builder: (context, state) => const Placeholder(),
+            ),
+          ],
+        ),
+      ],
+    );
+    return mainRouter;
+  }
 }
