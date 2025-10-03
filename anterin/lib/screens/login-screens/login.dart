c5584bcba7bf791fc74d3bbaaab77d5c470d43ed
Richard V.H <godof7sea@gmail.com>
2025-10-03 20:20:40 +0700

    Update login.dart
    agar ga overflow di hp cuman kek cut dan tambahin singlechildscrollfield

--- anterin/lib/screens/login-screens/login.dart
+++ anterin/lib/screens/login-screens/login.dart
@@ -29,10 +29,8 @@ class _LoginPageState extends State<LoginPage> {
   @override
   void initState() {
     super.initState();
-    setState(() {
-      _passController.clear();
-      _numController.clear();
-    });
+    _passController.clear();
+    _numController.clear();
   }
 
   @override
@@ -45,62 +43,66 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/anterin-logo-vertical.png',
-              width: 200,
-              height: 200,
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _numController,
-              decoration: InputDecoration(
-                labelText: 'Your Number / Nomor Hp Anda',
-                errorText: !_validate && _isPressed
-                    ? 'Nomor tidak boleh kosong!'
-                    : null,
-              ),
-            ),
-            TextField(
-              controller: _passController,
-              decoration: InputDecoration(
-                labelText: 'Password / Kata Sandi',
-                errorText: !_validate && _isPressed
-                    ? 'Kata sandi tidak boleh kosong!'
-                    : null,
-              ),
-            ),
-            const SizedBox(height: 10),
-            GestureDetector(
-              onTap: () {
-                context.pushNamed('mobile-number');
-              },
-              child: const Align(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  'Forgot password? / Lupa kata sandi?',
-                  style: TextStyle(color: Colors.blue),
+      body: SingleChildScrollView( 
+        child: SafeArea( 
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/anterin-logo-vertical.png',
+                  width: 200,
+                  height: 200,
                 ),
-              ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _numController,
+                  decoration: InputDecoration(
+                    labelText: 'Your Number / Nomor Hp Anda',
+                    errorText: _numController.text.isEmpty && _isPressed
+                        ? 'Nomor tidak boleh kosong!'
+                        : null,
+                  ),
+                ),
+                TextField(
+                  controller: _passController,
+                  decoration: InputDecoration(
+                    labelText: 'Password / Kata Sandi',
+                    errorText: _passController.text.isEmpty && _isPressed
+                        ? 'Kata sandi tidak boleh kosong!'
+                        : null,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                GestureDetector(
+                  onTap: () {
+                    context.pushNamed('mobile-number');
+                  },
+                  child: const Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'Forgot password? / Lupa kata sandi?',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(onPressed: _checkText, child: const Text('Login')),
+                const SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () {
+                    context.pushNamed('register');
+                  },
+                  child: const Text(
+                    "Don't have account? Register here\nTidak punya akun? Daftar disini",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(onPressed: _checkText, child: const Text('Login')),
-            const SizedBox(height: 20),
-            GestureDetector(
-              onTap: () {
-                context.pushNamed('register');
-              },
-              child: const Text(
-                "Don't have account? Register here\nTidak punya akun? Daftar disini",
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.blue),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
