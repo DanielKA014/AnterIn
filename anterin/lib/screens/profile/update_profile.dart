a7726a14dfcfce5eb4d5770d392df4961809ad43
Daniel <danielkevin0140@gmail.com>
2025-10-04 21:24:47 +0700

    mengubah update

--- anterin/lib/screens/profile/update_profile.dart
+++ anterin/lib/screens/profile/update_profile.dart
@@ -7,49 +7,57 @@ class UpdateProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Update Profile'),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Name',
-                border: OutlineInputBorder(),
+        child: Padding(
+          padding: EdgeInsetsGeometry.all(4),
+          child: Column(
+            children: [
+              Text('Update Profile'),
+              SizedBox(height: 60),
+              Column(
+                spacing: 8,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'No HP',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                    obscureText: true,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      border: OutlineInputBorder(),
+                    ),
+                    obscureText: true,
+                  ),
+                  ElevatedButton(
+                    child: const Text('Update'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
               ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'No HP',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Confirm Password',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              child: const Text('Update'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
