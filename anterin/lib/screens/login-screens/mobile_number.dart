f46086218e1c7af221b8c696c373d6a1cfc40997
Richard V.H <godof7sea@gmail.com>
2025-10-04 10:20:57 +0700

    upadate not clear

--- anterin/lib/screens/login-screens/mobile_number.dart
+++ anterin/lib/screens/login-screens/mobile_number.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class MobileNumberPage extends StatelessWidget {
+class MobileNumberPage extends StatefulWidget {
   const MobileNumberPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final controller = TextEditingController();
+  State<MobileNumberPage> createState() => _MobileNumberPageState();
+}
 
+class _MobileNumberPageState extends State<MobileNumberPage> {
+  final _numController = TextEditingController();
+  bool _validate = false;
+  bool _isPressed = false;
+
+  void _checkText() {
+    _isPressed = true;
+    setState(() {
+      _validate = _numController.text.isNotEmpty;
+    });
+
+    if (_validate) {
+      context.pushNamed('otp');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _numController.clear();
+  }
+
+  @override
+  void dispose() {
+    _numController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20),
@@ -21,17 +51,21 @@ class MobileNumberPage extends StatelessWidget {
             ),
             const SizedBox(height: 20),
             TextField(
-              controller: controller,
-              decoration: const InputDecoration(
+              controller: _numController,
+              decoration: InputDecoration(
                 labelText: 'Enter Mobile Number / Masukkan Nomor Ponsel',
+                errorText: _numController.text.isEmpty && _isPressed
+                    ? 'Nomor Ponsel tidak boleh kosong!'
+                    : null,
               ),
               keyboardType: TextInputType.phone,
+              onChanged: (text) => setState(() {
+                _isPressed = false;
+              }),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                context.pushNamed('otp');
-              },
+              onPressed: _checkText,
               child: const Text('Send OTP'),
             ),
           ],
